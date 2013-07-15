9705ab493c0ace8d447707ee185483e5661f4f87
tochi <tochi808@gmail.com>
2013-07-15 13:16:47 +0900

    顧客削除時に債権も同時削除するようにした

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,12 @@ class Customer < ActiveRecord::Base
   attr_accessible :name
 
   has_many :saikens
+
+  after_destroy :destroy_saikens
+
+  def destroy_saikens
+  	self.saikens.each do |saiken|
+  		saiken.destroy
+  	end
+  end
 end
